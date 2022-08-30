dcf6c2e0c40a8f02d367b678dc6a752f88f8ee2c
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-08-30 13:43:00 -0300

    project start

---
+++ Bank-app/Bank_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_appApp.swift
+//  Bank-app
+//
+//  Created by Lucas Newlands on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Bank_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
